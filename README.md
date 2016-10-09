46450bde512564c957f8a9feb03124c215203f9a
Christopher M. Wood <cwood@qualtrax.com>
2016-10-09 12:18:17 -0400

    Rudimentary non looping word replacement added

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-There Their They're Chrome Extension
+Your There Chrome Extension
 ====================================
+A super simple Chrome Extension that globally replaces all instances of the following words
+as follows on page load.
 
+- There   -> Their
+- Their   -> The're
+- They're -> There
+- Your    -> Your're
+- You're  -> Your
+
+Fully intended to annoy anybody that forgets it was added to their browser.
--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-var words = ["there", "they're"];
-var replacements = ["their", "there"];
+var flagMap = {};
+flagMap['there'] = 'dxt1';
+flagMap['There'] = 'Dxt1';
+flagMap['their'] = 'dxt2';
+flagMap['Their'] = 'Dxt2';
+flagMap['they\'re'] = 'dxt3';
+flagMap['They\'re'] = 'Dxt3';
+flagMap['your'] = 'dxt4';
+flagMap['Your'] = 'Dxt4';
+flagMap['you\'re'] = 'dxt5';
+flagMap['You\'re'] = 'Dxt5';
+
+var finalReplaceMap = {};
+finalReplaceMap['dxt1'] = 'their';
+finalReplaceMap['Dxt1'] = 'Their';
+finalReplaceMap['dxt2'] = 'they\'re';
+finalReplaceMap['Dxt2'] = 'They\'re';
+finalReplaceMap['dxt3'] = 'there';
+finalReplaceMap['Dxt3'] = 'There';
+finalReplaceMap['dxt4'] = 'you\'re';
+finalReplaceMap['Dxt4'] = 'You\'re';
+finalReplaceMap['dxt5'] = 'your';
+finalReplaceMap['Dxt5'] = 'Your';
+
+var pageBody = document.body.innerHTML;
 
 function replaceAllText() {
-	for (var i = 0; i < words.length; i++)
-		replaceText(words[i], replacements[i]);
-
-	//var paragraphs = document.getElementsByTagName("p");
-	//for (var i = 0; i < words.length; i++)
-	//	replaceText(words[i], replacements[i]);
+    
+    for (var key in flagMap) {
+        replaceText(key, flagMap[key]);
+    }
+    
+    for (var key in finalReplaceMap) {
+        replaceText(key, finalReplaceMap[key]);
+    }
+    
+    document.body.innerHTML = pageBody;
 }
 
 function replaceText(text, replacement) {
-	var regex = new RegExp(text, 'gi');
-	document.body.innerHTML = document.body.innerHTML.replace(regex, replacement);
+	var regex = new RegExp(text, 'g');
+	pageBody = pageBody.replace(regex, replacement);
 }
 
 replaceAllText();
